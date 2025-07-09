18a80490dd14170eb93902ccb33b4270a7ed3937
sereyriddhmeng6363 <sereyriddhmeng@gmail.com>
2025-07-09 10:38:05 +0700

    Update feature or bug fix description

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: './', // Or '/' if deploying at the root
   plugins: [vue()],
   css: {
     postcss: './postcss.config.js'
